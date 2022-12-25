40eab1f5eb856eaf5800aad511391f7bdd38d8d7
Szymon Dizewski <szymon.dizewski1@gmail.com>
2022-12-25 02:35:23 +0100

    Added servers and while loop

--- src/main.cpp
+++ src/main.cpp
@@ -13,18 +13,21 @@ int main(void) {
   Switch nswitch;
 
   HTTPServer server0(nswitch.connect("10.100.0.2"), "server0");
-  HTTPServer server1(nswitch.connect("10.100.0.3"), "server1");
-  HTTPServer server2(nswitch.connect("10.100.0.4"), "server2");
-  HTTPServer server3(nswitch.connect("10.100.0.5"), "server3");
-  HTTPServer server4(nswitch.connect("10.100.0.6"), "server4");
+  // HTTPServer server1(nswitch.connect("10.100.0.3"), "server1");
+  // HTTPServer server2(nswitch.connect("10.100.0.4"), "server2");
+  // HTTPServer server3(nswitch.connect("10.100.0.5"), "server3");
+  // HTTPServer server4(nswitch.connect("10.100.0.6"), "server4");
   
   HTTPClient client0(nswitch.connect("10.0.0.2"));
-  HTTPClient client1(nswitch.connect("10.0.0.3"));
-  HTTPClient client2(nswitch.connect("10.0.0.4"));
-  HTTPClient client3(nswitch.connect("10.0.0.5"));
+  // HTTPClient client1(nswitch.connect("10.0.0.3"));
+  // HTTPClient client2(nswitch.connect("10.0.0.4"));
+  // HTTPClient client3(nswitch.connect("10.0.0.5"));
 
   nswitch.start();
 
-
+  while(1){
+    ThisThread::sleep_for(1s);
+  }
+  
   return 0;
 }
